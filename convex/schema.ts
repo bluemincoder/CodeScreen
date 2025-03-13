6d79e220f4fafa513693e26e14816db2bc069505
minaal07 <130892773+minaal07@users.noreply.github.com>
2025-03-13 16:24:23 +0530

    Stream Setup

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,7 +5,7 @@ export default defineSchema({
     users: defineTable({
         name: v.string(),
         email: v.string(),
-        image: v.optional(v.string()),
+        image: v.optional(v.string()), 
         // password: v.string(),
         role: v.union(v.literal("candidate"), v.literal("interviewer")),
         clerkId: v.string(),
