f0d1889a915d64dfe14a29fa5bd74c972e4769e5
minaal07 <130892773+minaal07@users.noreply.github.com>
2025-03-18 14:05:22 +0530

    Created Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import { Button } from "@/components/ui/button";
-import {
-    SignInButton,
-    SignUpButton,
-    SignedIn,
-    SignedOut,
-    UserButton,
-} from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
+
 export default function Home() {
-    return <div className="m-10">
-        <SignInButton>
-            <Button>Sign In</Button>
-        </SignInButton>
-        </div>;
+    return (
+        <div className="m-10">
+            home page
+        </div>
+    );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+// import { CodeIcon } from "lucide-react";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import DasboardBtn from "./DasboardBtn";
+
+function Navbar() {
+    return (
+        <nav className="border-b">
+            <div className="flex h-16 items-center px-4 container mx-auto">
+                {/* LEFT SIDE -LOGO */}
+                <Link
+                    href="/"
+                    className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80 transition-opacity"
+                >
+                    <span className="text-4xl text-cyan-600">&lt;</span>
+                    <span className="bg-gradient-to-r from-cyan-600 to-emerald-600 bg-clip-text text-transparent">
+                        CodeScreen
+                    </span>
+                    <span className="text-4xl text-emerald-600">&gt;</span>
+                </Link>
+
+                {/* RIGHT SIDE - ACTIONS */}
+                <SignedIn>
+                    <div className="flex items-center space-x-4 ml-auto">
+                        <DasboardBtn />
+                        <ModeToggle />
+                        <UserButton />
+                    </div>
+                </SignedIn>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
